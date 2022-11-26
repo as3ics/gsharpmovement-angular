5c7c620a3f1847f4888c92a92aba5f390f9b9d80
as3ics <zach@as3ics.com>
2022-11-26 05:10:53 -0500

    v1.1.1 added custum ga events

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   ipApiUrl: 'https://ipgeolocation.abstractapi.com/v1/?api_key=fee738ba7e924d16954490fa52074510',
   apiUrl: 'https://api.gsharpmovement.com:3000',
-  downloadButtonText: 'Download Free Album Here'
+  downloadButtonText: 'Download Free Album Here',
+  ga: 'G-1SV1N6GKNW'
 };
