19d2860e790e8214b44491837fdd84b8b5d6a2ca
as3ics <zach@as3ics.com>
2022-11-26 09:10:51 -0500

    bing

--- src/app/components/pages/home-one/home-one.component.ts
+++ src/app/components/pages/home-one/home-one.component.ts
@@ -15,14 +15,15 @@ export class HomeOneComponent implements OnInit {
   buttonText: string = environment.downloadButtonText;
 
   constructor(private downloadService: DownloadService, private meta: Meta, private title: Title) {
-    this.title.setTitle("Free Album Giveaway of Gadfly & G# Movement's We Shall Prevail(2022 Remaster)");
+    this.title.setTitle("Free Album Giveaway of Gadfly & G# Movement's We Shall Prevail (2022 Remaster)");
     this.meta.addTags([
       { name: "description", content: "The home of Gadfly & G# Movement where they are giving away their new indie folk-rock album for free." },
       { name: "author", content: "Zach DeGeorge" },
       { name: "keywords", content: "Gadfly, G# Movement, indie, music, folk-rock, We Shall Prevail, Zach DeGeorge, Zachary DeGeorge, Samuel P Lauzon, Tobi Wats, Thiago Alves, Matt Mooney, Chase Holton, Doug Cowan, Timmy Jay, Domenick DeGeorge, The Usual Stoopkid, new music, free music" },
       { name: "robots", content: "index, follow" },
       { charset: "UTF-8" },
-      { name: "date", content: "2022-11-26", scheme: "YYYY-MM-DD" }
+      { name: "date", content: "2022-11-26", scheme: "YYYY-MM-DD" },
+      { name: "msvalidate.01", content: "A3AF8306380177A005ED45495E44FF9F" }
     ])
   }
 
