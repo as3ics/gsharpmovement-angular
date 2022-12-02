35d2b4dce6f3245a493d3a122394d006947f40db
as3ics <zach@as3ics.com>
2022-12-02 00:39:50 -0500

    prerender

---
+++ dist/gsharpmovement/browser/assets/album/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+arkb deploy `01 Life is a Journey (feat. Thiago Alves).mp3`
+arkb deploy `02 Crash (And Burn) [feat. Samuel P Lauzon].mp3`
+arkb deploy `03 Live Love (Live).mp3`
+arkb deploy `04 Yahweh (feat. Zachary DeGeorge).mp3`
+arkb deploy `05 Feel It (feat. Timmy Jay).mp3`
+arkb deploy `06 From Your Hand To Mine (Instrumental).mp3`
+arkb deploy `07 From Your Hand To Mine (feat. The Usual Stoopkid).mp3`
+arkb deploy `08 Another Day (feat. Domenick DeGeorge).mp3`
+arkb deploy `09 Every Day's Not Same (feat. Tobi Wats).mp3`
+arkb deploy `10 Gonna Shake (feat. Doug Cowan).mp3`
+arkb deploy `11 We Shall Prevail (feat. Chase Holton).mp3`
+arkb deploy `12 On My Way Home (feat. Matt Mooney).mp3`
+arkb deploy `13 We Shall Prevail (Endofempires Mix) [feat. Domenick DeGeorge].mp3`
+
