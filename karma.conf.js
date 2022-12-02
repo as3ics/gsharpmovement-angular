955adb9d2acea4e63d0e21480c78d1bc99e28d9f
as3ics <zach@as3ics.com>
2022-12-02 00:33:25 -0500

    ok

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/weshallprevail'),
+      dir: require('path').join(__dirname, './coverage/gsharpmovement'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'weshallprevail'`, () => {
+  it(`should have as title 'gsharpmovement'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('weshallprevail');
+    expect(app.title).toEqual('gsharpmovement');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('weshallprevail app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('gsharpmovement app is running!');
   });
 });
